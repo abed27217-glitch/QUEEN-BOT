7702bc2823b2fb6fbab2010bab12a3749fc6e07b
abed27217-glitch <abed27217@gmail.com>
2026-02-16 22:41:49 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # QueenBot main script
-
+from commands import start_command
 def start_bot():
-    print("QueenBot is running...")
+    print(start_command())
 
 if __name__ == "__main__":
     start_bot()
